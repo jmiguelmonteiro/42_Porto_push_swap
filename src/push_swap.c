dde56ab2b33609a6433653ba470fe55a5e63c6bd
Miguel Monteiro <jmmonteiro@gmail.com>
2025-05-29 11:45:45 +0000

    20250529

--- src/push_swap.c
+++ src/push_swap.c
@@ -14,25 +14,23 @@
 
 int	main(int argc, char **argv)
 {
-	t_list	**stack_a;
-	t_list	**stack_b;
+	t_list	*stack_a;
+	t_list	*stack_b;
 
-	stack_a = malloc(sizeof(**stack_a));
-	stack_b = malloc(sizeof(**stack_b));
-	*stack_a = NULL;
-	*stack_b = NULL;
+	stack_a = NULL;
+	stack_b = NULL;
 	if (argc < 2)
 		return (0);
 	check_arguments(argc, argv);
-	if (!init_stack(argc, argv, stack_a))
+	if (!init_stack(argc, argv, &stack_a))
 		ft_error();
 
-	if (is_sorted(*stack_a))
+	if (is_sorted(stack_a))
 		ft_printf("Sorted!!!\n");
 		
-//	operations_tests(stack_a, stack_b);
+//	operations_tests(&stack_a, &stack_b);
 	
-	free_stack(stack_a);
-	free_stack(stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
 	exit (0);
 }
